7b0032611985aeb9a29630ea351678477a53177a
LautaroMTS <lautaromartinmontes7@gmail.com>
2024-03-20 18:46:26 -0300

    Se agrega el Proyecto

---
+++ IdiomaP.js
@@ -0,0 +1,28 @@
+const readline = require('readline');
+
+function main() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question('Ingrese la frase a traducir: ', (frase) => {
+        const traduccion = traducirFrase(frase);
+        console.log(traduccion);
+        rl.close();
+    });
+}
+
+function traducirFrase(frase) {
+    let traduccion = '';
+    for (let i = 0; i < frase.length; i++) {
+        const letra = frase.charAt(i);
+        if ('aeiou'.includes(letra)) {
+            traduccion += letra + 'p' + letra;
+        } else {
+            traduccion += letra;
+        }
+    }
+    return traduccion;
+}
+
